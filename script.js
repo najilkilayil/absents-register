1dbe50e1a1121f680d0eb6faff656da61ed38988
Najil <najiljan26@gmail.com>
2026-06-28 21:36:25 +0530

    Made the table for dynamic

--- script.js
+++ script.js
@@ -4,17 +4,45 @@ let textEl = document.getElementById("name-el")
 let saveBtn = document.getElementById("save-btn")
 let tableEl = document.getElementById("table-el")
 
-saveBtn.addEventListener("click", function() {
+let values = {}
+
+saveBtn.addEventListener("click", function () {
     let date = dateEl.value
     console.log(date)
 
     let name = textEl.value
     console.log(name)
 
-    tableEl.innerHTML += `
-        <tr>
-            <td>${date}</td>
-            <td>${name}</td>
-        </tr>
-    `
+    if (!values[date]) {
+        values[date] = []
+    }
+
+    values[date].push(name)
+
+    textEl.value = ""
+
+    tableEl.innerHTML = ""
+    for (let date in values) {
+        let first = true
+
+        for (let name of values[date]) {
+            if (first) {
+                tableEl.innerHTML += `
+                <tr>
+                    <td>${date}</td>
+                    <td>${name}</td>
+                </tr>
+                `
+
+                first = false
+            } else {
+                tableEl.innerHTML += `
+                <tr>
+                    <td></td>
+                    <td>${name}</td>
+                </tr>
+                `
+            }
+        }
+    }
 })
